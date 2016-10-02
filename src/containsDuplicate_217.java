760a80fc92425487afe32677032647f7ea33eb51
Zhaolian Zhou <u5782545@anu.edu.au>
2016-10-03 07:11:11 +1100

    No 217, 2016-10-3

--- src/containsDuplicate_217.java
+++ src/containsDuplicate_217.java
@@ -1,5 +1,13 @@
 import java.util.HashSet;
-
+/**
+ * Problem No : 217
+ * Description:
+ * Given an array of integers, find if the array contains any duplicates. 
+ * Your function should return true if any value appears at least twice in the array, 
+ * and it should return false if every element is distinct.
+ * @author Zhaolian
+ *
+ */
 public class containsDuplicate_217 {
 	public boolean containDuplicate(int[] nums){
 		HashSet<Integer> currNum = new HashSet<Integer>();
