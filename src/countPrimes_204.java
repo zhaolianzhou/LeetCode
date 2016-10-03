cda9ae1785db9b1043493327240eca03ebd17f0b
Zhaolian Zhou <u5782545@anu.edu.au>
2016-10-03 11:12:14 +1100

    No 204, 2016-10-3

---
+++ src/countPrimes_204.java
@@ -0,0 +1,43 @@
+
+public class countPrimes_204 {
+	public static int countPrimes(int n){
+		int result = 0;
+		for(int i = 1; i < n; i++){
+			if(isPrime(i))
+				result++;
+		}
+		return result;
+	}
+	public static int countPrimes2(int n){
+		int result = 0; 
+		boolean[] isPrime = new boolean[n];
+		for(int i = 2; i< n; i++)
+			isPrime[i] = true;
+		
+		for(int i = 2; i < Math.sqrt(n); i++){
+			if(!isPrime[i])
+				continue;
+			for(int j = i*i; j < n; j+=i){
+				isPrime[j] = false;
+			}
+		}
+		for(int i = 2; i < n; i++){
+			if(isPrime[i])
+				result++;
+		}
+		return result++;
+	}
+	public static boolean isPrime(int num){
+		if(num <=1)
+			return false;
+		for(int i = 2; i <= Math.sqrt(num); i++){
+			if(num%i==0)
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(countPrimes(1500000));
+	}
+}
