f2d6f2525eaeb084f98b72e1b0a6f50efc0799af
ZhaolianZhou <zhaolianzhou92@gmail.com>
2017-01-08 22:54:41 +1100

    contest14/magicalString

--- src/contest/weekly14/magicalString.java
+++ src/contest/weekly14/magicalString.java
@@ -1,12 +1,70 @@
 package contest.weekly14;
 
+import java.util.ArrayList;
+
 public class magicalString {
-	public int magicalString(int n){
-		return 0;
+	public static int magicalString(int n){
+		if(n==0)
+			return 0;
+		ArrayList<Integer> S = new ArrayList<Integer>();
+		S.add(1);
+		S.add(2);
+		int currIndex=1;
+		int previous1=1;
+		int previous2=2;
+		int count=S.size();
+		int count1=1;
+		boolean is2Pre=true;
+		System.out.println(S);
+		while(count<n){
+			if(S.get(currIndex)==1){
+				currIndex++;
+				if(previous2==1)
+					S.add(2);
+				else
+				{
+					S.add(1);
+					count1++;
+				}
+			}
+			else{
+				if(previous1==previous2){
+					currIndex++;
+					if(previous2==1)
+						S.add(2);
+					else{
+						S.add(1);
+						count1++;
+					}
+				}
+				else{
+					if(is2Pre==false){
+						if(previous2==1)
+							S.add(2);
+						else
+						{
+							S.add(1);
+							count1++;
+						}
+					}
+					else{
+						if(previous2==1)
+							count1++;
+						S.add(previous2);
+					}
+				}
+				is2Pre=!is2Pre;
+			}
+			count++;
+			previous1=S.get(count-2);
+			previous2=S.get(count-1);	
+		}
+		System.out.println(S);
+		return count1;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(magicalString(6));
 	}
 
 }
---
+++ src/findNumbersDisappearedInArray_448.java
@@ -0,0 +1,14 @@
+/**
+ * Find All Numbers Disappeared in an Array
+ * Note: in O(n) runtime and no extra space
+ * @author zhaolianzhou
+ *
+ */
+public class findNumbersDisappearedInArray_448 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
